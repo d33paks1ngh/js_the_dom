da37a74eb3ccc000bd409657174a6e1fa4d5b388
Deepak Singh <ds8853845715@gmail.com>
2024-11-14 07:13:22 +0530

    que1

---
+++ one.js
@@ -0,0 +1,5 @@
+var btn = document.querySelector("button");
+var p = document.querySelector("p");
+btn.addEventListener("click", function () {
+  p.textContent = "text of paragraph is changed";
+});
